8f5e2222a73ad071a9a0428ba3c28bb75cf71305
Tera <24725862+teraa@users.noreply.github.com>
2022-05-17 16:04:01 +0200

    add sessions endpoint

---
+++ Kino/Features/Sessions/Actions/Create.cs
@@ -0,0 +1,70 @@
+﻿using FluentValidation;
+using JetBrains.Annotations;
+using Kino.Services;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kino.Features.Sessions.Actions;
+
+public static class Create
+{
+    public record Command(Model Model)
+        : IRequest<IActionResult>;
+
+    public record Model(
+        string Name,
+        string Password);
+
+    [UsedImplicitly]
+    public class ModelValidator : AbstractValidator<Model>
+    {
+        public ModelValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Password).NotEmpty();
+        }
+    }
+
+    [PublicAPI]
+    public record Result(
+        int Id,
+        string Token);
+
+    [UsedImplicitly]
+    public class Handler : IRequestHandler<Command, IActionResult>
+    {
+        private readonly KinoDbContext _ctx;
+        private readonly PasswordService _passwordService;
+        private readonly TokenService _tokenService;
+
+        public Handler(
+            KinoDbContext ctx,
+            PasswordService passwordService,
+            TokenService tokenService)
+        {
+            _ctx = ctx;
+            _passwordService = passwordService;
+            _tokenService = tokenService;
+        }
+
+        public async Task<IActionResult> Handle(Command request, CancellationToken cancellationToken)
+        {
+            var normalizedUsername = request.Model.Name.ToLowerInvariant();
+
+            var entity = await _ctx.Users
+                .AsNoTracking()
+                .Where(x => x.Name == normalizedUsername)
+                .FirstOrDefaultAsync(cancellationToken);
+
+            if (entity is null || !_passwordService.Test(request.Model.Password, entity.PasswordHash, entity.PasswordSalt))
+                return new BadRequestResult();
+
+            var token = _tokenService.CreateToken(entity.Id);
+
+            var result = new Result(entity.Id, token);
+
+            return new OkObjectResult(result);
+        }
+    }
+}
---
+++ Kino/Features/Sessions/SessionsController.cs
@@ -0,0 +1,22 @@
+﻿using Kino.Features.Sessions.Actions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Kino.Features.Sessions;
+
+[ApiController]
+[Route("[controller]")]
+public class SessionsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public SessionsController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Create.Result), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Create(Create.Model model, CancellationToken cancellationToken)
+        => await _mediator.Send(new Create.Command(model), cancellationToken);
+}
